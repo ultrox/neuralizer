f89f113c3f8b29f8d3f1c0915af4f1b716b36b4e
ultrox <crashxx@gmail.com>
2022-03-19 22:25:24 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# neuralizer
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-hyuovs)
---
+++ app/routes/index.tsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from 'react';
+
+import styles from '~/styles/index.css';
+
+export function links() {
+  return [{ rel: 'stylesheet', href: styles }];
+}
+
+export default function Index() {
+  const [flash, setFlash] = useState(false);
+  const [showMessage, setShowMsg] = useState(false);
+
+  const setFlashOn = () => {
+    setFlash(true);
+  };
+  const setFlashOff = () => setFlash(false);
+
+  useEffect(() => {
+    let id: NodeJS.Timeout;
+    id = setTimeout(() => {
+      setFlashOff();
+    }, 300);
+    return () => clearTimeout(id);
+  }, [flash]);
+
+  useEffect(() => {
+    let id: NodeJS.Timeout;
+    id = setTimeout(() => {
+      setShowMsg(false);
+    }, 3000);
+    return () => clearTimeout(id);
+  }, [showMessage]);
+
+  const turnNeuralizorOn = () => {
+    setFlashOn();
+    setShowMsg(true);
+  };
+  const cssForFlash = flash ? 'block' : 'none';
+
+  return (
+    <>
+      <div className="flash" style={{ display: cssForFlash }} />
+      <div className="parent">
+        {showMessage ? (
+          <div className="message">
+            <p>Covid Is Over</p>
+            <p>You Love Ukraine NOW!!</p>
+          </div>
+        ) : (
+          <div className="dot">
+            <button onClick={turnNeuralizorOn} className="flashButton"></button>
+          </div>
+        )}
+      </div>
+    </>
+  );
+}
